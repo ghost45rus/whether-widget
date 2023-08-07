3ba31d0d7559be7bc8f59707e8289c80f353b5d4
Paul <ghostkurgan45@gmail.com>
2023-08-07 16:34:17 +0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>VUE</h1>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  mounted(){
+    // "lon": ,
+    //     "lat": 51.5085
+
+    this.axios.get('https://api.ipify.org?format=json')
+    // http://ip-api.com/json/24.48.0.1
+    if ("geolocation" in navigator) {
+      /* местоположение доступно */
+      navigator.geolocation.getCurrentPosition((r) => {
+        console.log(r)
+        this.axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${r.coords.latitude}&lon=${r.coords.longitude}&exclude=daily&units=metric&appid=b2bcef1b83f8b44be62163b5f7bf64da`).then(response => {
+          let data = response.data
+          console.log(data)
+          console.log('City: ', data.name)
+          console.log('Visibility: ', data.visibility)
+          console.log(`Wind: deg:${data.wind.deg} gust: ${data.wind.gust} speed: ${data.wind.speed}`)
+          console.log(`Weather: ${data.weather[0].description}`)
+          console.log(`feels_like: ${data.main.feels_like} humidity: ${data.main.humidity} pressure: ${data.main.pressure} temp: ${data.main.temp} ` )
+        })
+      } )
+
+    } else {
+      /* местоположение НЕ доступно */
+    }
+
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
